8be63c51a5e183c64ed7c5b8ff1e29d0143a603e
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-05-16 21:59:02 -0400

    initial push

---
+++ portfolio_website-STARTER/src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Professional Portfolio
+      </SectionTitle>
+      <SectionText>
+        Welcome to my portfolio! Here is where you can view my accomplishments, find my social media accounts and more! <br />
+        Some technologies I know / worked with include: JavaScript, JSX, JQuery, MySQL, CSS, HTML, MongoBD, REACT, and others. I would love to help with you or your company with your next project.
+      </SectionText>
+      <Button onClick={() => window.location = "rebekahmcdevitt@gmail.com"}>Contact Me</Button>
+  </LeftSection>
+  </Section >
+);
+
+export default Hero;
