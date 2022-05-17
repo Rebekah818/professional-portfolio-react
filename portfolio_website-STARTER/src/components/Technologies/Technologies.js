e44d73ce6846fc9cae871b70a81f690961b5912a
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-05-16 22:11:59 -0400

    added tech info and icons to technologies section

--- portfolio_website-STARTER/src/components/Technologies/Technologies.js
+++ portfolio_website-STARTER/src/components/Technologies/Technologies.js
@@ -4,9 +4,35 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have worked with many front and back end technologies in web development. I specifically have worked with the MERN tech stack. 
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="4rem" />
+        <ListContainer>
+          <ListTitle>Front End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+            </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="4rem" />
+        <ListContainer>
+          <ListTitle>Back End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js and MYSQL
+            </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
